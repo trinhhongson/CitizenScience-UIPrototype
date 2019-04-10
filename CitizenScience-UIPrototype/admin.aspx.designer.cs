d9f5ca4c51c3307bb2b6ead830b455bff7ef98d5
TrailMixBoi <tuf67637@temple.edu>
2019-04-10 11:51:00 -0400

    new mods

--- CitizenScience-UIPrototype/admin.aspx.designer.cs
+++ CitizenScience-UIPrototype/admin.aspx.designer.cs
@@ -184,6 +184,15 @@ namespace CitizenScience_UIPrototype {
         protected global::System.Web.UI.WebControls.Button btnUploadSensorData;
         
         /// <summary>
+        /// btnFileFormat control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnFileFormat;
+        
+        /// <summary>
         /// divUploadError control.
         /// </summary>
         /// <remarks>
